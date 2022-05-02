e1590a867eaeabc20cb7ffc007d73ca1eef916a3
wilway256 <wf.roser@gmail.com>
2022-05-02 10:00:16 -0700

    Move analysis loop to main

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ import src.model as m
 import openseespy.opensees as ops
 from src.excel_to_database import initialize_database
 from src.bookkeeping import make_output_directory, save_input_file
-from src.analysis import analysis_loop
+from src.analysis import constant_analysis
+import os
 
 ops.wipe()
 
@@ -41,12 +42,10 @@ m.define_transformations(db)
 m.make_elements(db)
 
 # %% Manual Loads
-# print('Applying Loads')
-# # m.apply_loads()
+# For debugging and quick testing
 
-
-# ops.timeSeries('Linear', 1)
-# ops.pattern('Plain', 1, 1)
+# ops.timeSeries('Linear', 999)
+# ops.pattern('Plain', 999, 999)
 # # nodeList = [2]
 # # loadList = [8.0]
 # # for node, load in zip(nodeList, loadList):
@@ -56,35 +55,40 @@ m.make_elements(db)
 # # ops.load(db.get_node_tag('F2 Corner4'), *[0.0, -1000.0, 0.0, 0.0, 0.0, 0.0])
 # # ops.load(db.get_node_tag('F3 Center'), *[1000.0, 0.0, 0.0, 0.0, 0.0, 0.0])
 
-# # %% Loads
-# # load.gravity
-# # load.
 
-
-
-# %% Analysis
+# %% Load Cases
 print("Analysis Loop")
-analysis_loop(db, out_dir)
+loadCases = db.loadCase.query('Run == "Y"')
+
+for case in loadCases.index:
+    # Make output subfolders
+    try:
+        os.mkdir(out_dir + '/' + case)
+    except FileExistsError:
+        pass
+    
+    # Recorders
+    
+    
+    # Gravity (or other constant load case)
+    print('Applying Gravity')
+    constant_analysis(db, 'gravity')
+    
+    # Static
+    if loadCases['Type'][case] == 'Static':
+        print("static if")
+    
+    # Pushover (displacement control)
+    elif loadCases['Type'][case] == 'Disp':\
+        print("disp ctrl if")
+    
+    # Dynamic
+    elif loadCases['Type'][case] == 'GM':
+        print("dynamic if")
+        pass
+    
+    
 print('Out of Loop')
-# print('Analysis Options')
-# # m.set_analysis_options()
-# ops.system("BandSPD")
-# ops.numberer("Plain")
-# ops.constraints("Transformation")
-# ops.test('EnergyIncr', 1e-6, 100, 1)
-# ops.integrator("LoadControl", 0.1, 10)
-# ops.algorithm("KrylovNewton")
-# ops.analysis("Static")
-
-# # m.run_analysis()
-# a = ops.analyze(1)
-
-# # %% Recorders
-# # #r.make_node_rocorders(db)
-# # #r.make_ele_recorders(db)
-# # ops.recorder('Node', '-xml', out_dir + 'eigen.xml', '-node', 24, 22, '-dof', 1, 2, 6, 'eigen 0')
-# ops.eigen(2)
-# # ops.record()
 
 # # ops.remove('recorders')
 # # ops.wipe()
