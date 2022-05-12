10a89d9504e00ab4c311fa0b4912bf9412cf0fb9
wilway256 <wf.roser@gmail.com>
2022-05-12 15:16:11 -0700

    New Feature: xml parsing

--- postprocessing.py
+++ postprocessing.py
@@ -9,10 +9,10 @@ Created on Mon Nov 15 11:24:08 2021
 @author: wroser
 """
 
-import openseespy.opensees as ops
-import opsvis
-import src.postprocessing_active as pp1
-import src.postprocessing_saved as pp2
+# import openseespy.opensees as ops
+# import opsvis
+# import src.postprocessing_active as pp1
+# import src.postprocessing_saved as pp2
 
 # %% Postprocessing
 # nodes = ops.getNodeTags()
@@ -31,4 +31,41 @@ import src.postprocessing_saved as pp2
 # opsvis.plot_defo(interpFlag=0)
 # opsvis.plot_defo( az_el=(-60.0, 30.0) )
 
-opsvis.plot_mode_shape(1, interpFlag=0)
+# opsvis.plot_mode_shape(1, interpFlag=0)
+
+# %% XML Parser
+import xml.etree.ElementTree as ET
+import numpy as np
+import pandas as pd
+
+
+outdir = 'out/triangle/'
+file = 'node_t.xml'
+filepath = outdir + file
+tree = ET.parse(filepath)
+root = tree.getroot()
+
+column_names = []
+
+for element in root.iter():
+    # print(element.tag)
+    if element.tag == 'TimeOutput':
+        column_names.append(element[0].text)
+        print('    ', element[0].text)
+    elif element.tag == 'NodeOutput':
+        nodeTag = element.attrib['nodeTag']
+        for response in range(len(element)):
+            column_names.append('Node_' + nodeTag + ' ' + element[response].text)
+            print('    ', 'Node', nodeTag, element[response].text)
+
+
+data = root.find('Data').text
+data = data.strip().split('\n')
+for i in range(len(data)):
+    data[i] = data[i].split()
+    for j in range(len(data[i])):
+        data[i][j] = float(data[i][j])
+data = np.asarray(data)
+
+df = pd.DataFrame(data, columns=column_names)
+
