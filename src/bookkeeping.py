accf9be79dbfc97039d866a10e9526d9048543fc
wilway256 <wf.roser@gmail.com>
2022-05-13 17:22:18 -0700

    bugfix
    Removed selective sheet saving in favor of saving the original model file to avoid openpyxl bugs and needing to update a list of sheets to save.

--- src/bookkeeping.py
+++ src/bookkeeping.py
@@ -30,7 +30,8 @@ def make_output_directory(name=None):
                             'name or press enter to overwrite the existing '
                             'temp folder.\n')
         if newfilename == '':
-            shutil.rmtree('out/' + filename)
+            newfilename = 'temp'
+            shutil.rmtree('out/temp')
             os.mkdir('out/' + 'temp')
         else:
             try:
@@ -38,23 +39,33 @@ def make_output_directory(name=None):
             except FileExistsError:
                 shutil.rmtree('out/' + newfilename)
                 os.mkdir('out/' + newfilename)
+        filename = newfilename
     return 'out/' + filename + '/', filename
 
 def save_input_file(name, output_directory):
-    keep_these = ['nodes', 'nodeFix', 'nodeMass', 'diaphragms', 'elements', 'eleProperties', 'eleTransf']
-    input_wb = xl.load_workbook(name, keep_vba=False, data_only=True)
-    out_wb = xl.Workbook()
-    for sheet in input_wb.sheetnames:
-        if sheet in keep_these:
-            input_wb[sheet]._parent = out_wb
-            out_wb._add_sheet(input_wb[sheet])
-        else:
-            pass
-    del out_wb['Sheet'] # This is a default sheet created by openpyxl.
-    i_file_ext = -1*(len(name) - name.index('.'))
-    out_wb.save('out/' + output_directory + '/' + name[:i_file_ext] + '.xlsx')
-    out_wb.close()
-    input_wb.close()
+    # keep_these = ['nodes',
+    #               'nodeFix',
+    #               'nodeMass', 
+    #               'diaphragms',
+    #               'elements',
+    #               'eleProperties',
+    #               'eleTransf',
+    #               'recorders']#, 'nodeLoads', 'loadCases', 'recorders']
+    # input_wb = xl.load_workbook(name, keep_vba=False, data_only=True, read_only=True)
+    # out_wb = xl.Workbook()
+    # for sheet in input_wb.sheetnames:
+    #     if sheet in keep_these:
+    #         input_wb[sheet]._parent = out_wb
+    #         out_wb._add_sheet(input_wb[sheet])
+    #     else:
+    #         pass
+    # del out_wb['Sheet'] # This is a default sheet created by openpyxl.
+    # i_file_ext = -1*(len(name) - name.index('.'))
+    # out_wb.save('out/' + output_directory + '/' + name[:i_file_ext] + '.xlsx')
+    # out_wb.close()
+    # input_wb.close()
+    
+    shutil.copy(name, 'out/' + output_directory)
 
 def _check_if_exists(file):
     pass
