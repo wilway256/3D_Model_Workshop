53f0f4097a4a42de1367298ece04e9a0574ab6ba
wilway256 <wf.roser@nevada.unr.edu>
2022-09-23 18:12:54 -0700

    Total revamp
    Added multi variable functionality.

--- xml_to_graph.py
+++ xml_to_graph.py
@@ -1,111 +1,140 @@
 # -*- coding: utf-8 -*-
 """
-Created:  Thu May 12, 2022
+Created:  Fri Sep 23 14:41:19 2022
 Author:   wroser
 =============================================================================
 Description:
-
+    Overhaul of xml_to_graph.py.
 """
 
-import os
+# import os
 import tkinter as tk
 from tkinter import ttk
 import matplotlib as mpl
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
-import xml.etree.ElementTree as ET
-import numpy as np
+# import xml.etree.ElementTree as ET
+# import numpy as np
 import pandas as pd
-from postprocessing import xml_to_df
+from src.postprocessing import xml_to_df
+from functools import partial
 
+# Class definition for tkinter window
 class App(tk.Tk):
     def __init__(self):
+        
+        # Iterables
+        self.data = {}
+        self.hierarchy = {}
+        self.listbox = {}
+        self.varFrame = {}
+        
+        # Data
+        
+        self.data['X'] = pd.DataFrame()
+        self.data['Y'] = pd.DataFrame()
+        
+        
+        
+        # Basic Window Layout
         super().__init__() # Initializes Tk() window
         self.title('Embedded Graph')
         self.minsize(600, 400)
-        self.df = pd.DataFrame()
-        self.lFrame = ttk.Frame(self)
-        self.rFrame = ttk.Frame(self)
-        self.lFrame.pack(side=tk.LEFT, expand=True, fill='both', padx=10, pady=10)
-        self.rFrame.pack(side=tk.RIGHT, padx=10, pady=10)
+        
+        self.frame_left = ttk.Frame(self)
+        self.frame_left.pack(side=tk.LEFT, expand=True, fill='both')
+        self.frame_ui = ttk.Notebook(self.frame_left)
+        self.frame_plot = ttk.Frame(self)
+        self.frame_ui.pack(side=tk.TOP, expand=True, fill='both', padx=10, pady=10, anchor=tk.N)
+        self.frame_plot.pack(side=tk.RIGHT, padx=10, pady=10)
+        
+        self.variables = ttk.Frame(self.frame_ui)
+        # self.ui_lower = ttk.Frame(self.frame_left).pack()
+        self.plot_options = ttk.Frame(self.frame_ui)
+        self.frame_ui.add(self.variables, text=('Variables'))
+        self.frame_ui.add(self.plot_options, text=('Plot Options'))
         
         # Matplotlib figure
         self.fig = mpl.figure.Figure(figsize=(10, 8), dpi=100)
         self.ax = self.fig.add_subplot(1, 1, 1)
-        self.canvas = FigureCanvasTkAgg(self.fig, master=self.rFrame)
+        self.ax.grid()
+        self.canvas = FigureCanvasTkAgg(self.fig, master=self.frame_plot)
         self.canvas.draw()
         self.canvas.get_tk_widget().pack(side=tk.BOTTOM)
-        self.toolbar = NavigationToolbar2Tk(self.canvas, self.rFrame)
+        self.toolbar = NavigationToolbar2Tk(self.canvas, self.frame_plot)
         self.toolbar.update()
         self.canvas.get_tk_widget().pack(side=tk.BOTTOM)
         
-        # X-axis selection
-        self.selectFrame = ttk.LabelFrame(self.lFrame, text='text', padding=10)
-        self.selectFrame.pack(side=tk.TOP, expand=True, fill='both')
-        # ttk.Label(self.selectFrame, text="Hello World!").pack()
-        ttk.Button(self.selectFrame, text="Pick File", command=self.pick_file).pack(side=tk.BOTTOM)
-        # ttk.Button(self.lFrame, text="File", command=get_file).pack(side=tk.BOTTOM)
-        ttk.Button(self.lFrame, text="Plot", command=self.update_plot).pack()
-        self.listbox = tk.Listbox(self.selectFrame, selectmode='multiple', width=30, height=30)
-        for name in self.df.columns:
-            self.listbox.insert('end', name)
-        self.listbox.pack()
+        # Make two frames for selecting two variables to graph
         
-        # Treeview
-        # self.tree = ttk.Treeview(self.selectFrame)
-        # self.tree.pack()
+        
+        
+        # X-axis selection
+        for var in ['X', 'Y']:
+            self.varFrame[var] = ttk.Frame(self.variables)
+            self.varFrame[var].pack(side=tk.LEFT)
+            # self.frame_ui.add(self.varFrame[var], text=(var+'-axis')) # Adds tab to ttk notebook
+            
+            # Listbox for selecting data
+            selectFrame = ttk.LabelFrame(self.varFrame[var], text=var, padding=10)
+            selectFrame.pack(side=tk.TOP, expand=True, fill='both')
+            
+            self.listbox[var] = tk.Listbox(selectFrame, selectmode='multiple', width=30, height=30, exportselection=0)
+            for name in self.data[var].columns:
+                self.listbox[var].insert('end', name)
+            self.listbox[var].pack()
+                    
+            
+            # Buttons
+            ttk.Button(selectFrame, text="Pick File", command=partial(self.pick_file, var)).pack(side=tk.BOTTOM)
+        
+        
+        
+        ttk.Button(self.frame_left, text="Plot", command=self.update_plot).pack(side=tk.BOTTOM, expand=True)
+            
         
     
     def update_plot(self):
-        columns = [self.df.columns[i] for i in self.listbox.curselection()]
+        # Wipe current plot
         self.ax.clear()
-        for i in columns:
-            self.ax.plot(self.df['time'], self.df[i],
-                         linewidth=0.5)
-            self.ax.grid()
-            self.canvas.draw()
-    #     print(self.listbox.curselection(), results)
-    #     return results
-        # print(self.tree.selection())
+        self.ax.grid()
+        # Get data
+        colsX = [self.data['X'].columns[i] for i in self.listbox['X'].curselection()]
+        colsX = ['time'] if colsX == [] else colsX
+        colsY = [self.data['Y'].columns[i] for i in self.listbox['Y'].curselection()]
+        
+        if len(colsX) != len(colsY) and len(colsX) != 1:
+            print('Must select the same number of variables or only one x-variable.')
+            return
+        
+        # One independent variable
+        if len(colsX) == 1:
+            for headerY in colsY:
+                self.ax.plot(self.data['X'][colsX[0]],
+                             self.data['Y'][headerY],
+                              linewidth=0.5)
+                # self.ax.grid()
+                self.canvas.draw()
+            
+            
+            
+            
+        # Multiple independent variables
+        else:
+        
+            for headerX, headerY in zip(colsX, colsY):
+                self.ax.plot(self.data['X'][headerX],
+                             self.data['Y'][headerY],
+                              linewidth=0.5)
+                # self.ax.grid()
+                self.canvas.draw()
     
-    def pick_file(self):
+    def pick_file(self, var):
         filepath = tk.filedialog.askopenfilename(initialdir='./out', filetypes=[('XML Recorder', '*.xml')])
-        self.df, self.hierarchy, _ = xml_to_df(filepath)
-        print(filepath)
-        # tree = ET.parse(filepath)
-        # root = tree.getroot()
-        
-        # # Parse data labels
-        # column_names = []
-        
-        
-        # for element in root.iter():
-        #     # print(element.tag)
-        #     if element.tag == 'TimeOutput':
-        #         column_names.append(element[0].text)
-        #     elif element.tag == 'NodeOutput':
-        #         nodeTag = element.attrib['nodeTag']
-        #         for response in range(len(element)):
-        #             column_names.append('Node_' + nodeTag + ' ' + element[response].text)
-        
-        # # Parse numerical data from recorder, return numpy array
-        # data = root.find('Data').text
-        # data = data.strip().split('\n')
-        # for i in range(len(data)):
-        #     data[i] = data[i].split()
-        #     for j in range(len(data[i])):
-        #         data[i][j] = float(data[i][j])
-        # data = np.asarray(data)
-        
-        # Assign to class dataframe
-        # self.df = pd.DataFrame(data, columns=column_names)
-        self.listbox.delete(0, tk.END)
-        for name in self.df.columns:
-            self.listbox.insert('end', name)
-       
-        # for name, i in zip(self.hierarchy, range(len(self.hierarchy))):
-        #     self.tree.insert('', i, name, text=name)
-        #     for response in self.hierarchy[name]:
-        #         self.tree.insert(name, 'end', name + ' ' + response, text=response)
+        self.data[var], self.hierarchy[var], _ = xml_to_df(filepath)
+        print(var, filepath)
+        self.listbox[var].delete(0, tk.END)
+        for name in self.data[var].columns:
+            self.listbox[var].insert('end', name)
 
 if __name__ == '__main__':
     app = App()
